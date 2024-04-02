6719392ffaf71e7c94c4e2b49196d0210bf97e14
syurinn <165797419+syurinn@users.noreply.github.com>
2024-04-02 21:16:45 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,54 @@
+/*
+let myHeading = document.querySelector("h1"); //let，var声明一个变量
+myHeading.textContent = "Hello world!";
+*/
+
+/*
+let iceCream = "chocolate";
+if (iceCream === "chocolate") {
+  alert("我最喜欢巧克力冰淇淋了。");
+} else {
+  alert("但是巧克力才是我的最爱呀……");
+}
+*/
+
+/*
+document.querySelector("html").addEventListener("click", function () {
+    alert("别戳我，我怕疼");
+});
+*/
+
+let myImage = document.querySelector("img");
+myImage.onclick = function () {
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/test1.jpg") {
+    myImage.setAttribute("src", "images/test2.jpg");
+  } else {
+    myImage.setAttribute("src", "images/test1.jpg");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    let myName = prompt("请输入你的名字。");
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = "Mozilla 酷毙了，" + myName;
+    }
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem("name");
+    myHeading.textContent = "Mozilla 酷毙了，" + storedName;
+}
+
+myButton.onclick = function () {
+    setUserName();
+};
+  
